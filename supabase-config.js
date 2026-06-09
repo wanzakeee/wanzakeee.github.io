947b4ba51c56051721554e68bb7df58b7ae48969
wanzakeee <amir885158@gmail.com>
2026-06-09 21:47:32 +0500

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// Конфигурация Supabase
+const SUPABASE_URL = 'https://sqzmtrweyzbtbuezanbd.supabase.co'; // Замени на свой URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxem10cndleXpidGJ1ZXphbmJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMTU3MzUsImV4cCI6MjA5NjU5MTczNX0.4Ucz1geyk1TyphfMzrmhkMwnDVIL7vODyPO8fayP-ZU'; // Замени на свой ключ
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
